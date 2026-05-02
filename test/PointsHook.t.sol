7cf7c81266a2a8356bb192db4f49578e7c7132a4
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2026-05-02 13:40:10 +0800

    updated

---
+++ test/PointsHook.t.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {PoolSwapTest} from "@uniswap/v4-core/src/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+
+import {PoolManager} from "v4-core/src/PoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {SwapParams, ModifyLiquidityParams} from "v4-core/src/types/PoolOperation.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/src/types/Currency.sol";
+import {PoolId} from "@uniswap/v4-core/src/types/PoolId.sol";
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {SqrtPriceMath} from "v4-core/src/libraries/SqrtPriceMath.sol";
+import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+
+import {ERC1155TokenReceiver} from "solmate/src/tokens/ERC1155.sol";
+
+import {console} from "forge-std/console.sol";
+import {Test} from "forge-std/Test.sol";
+
+import {PointsHook} from "../src/PointsHook.sol";
+
+contract TestPointsHook is Test, Deployers, ERC1155TokenReceiver {
+
+    MockERC20 token;
+
+    Currency ethCurrency = Currency.wrap(address(0));
+    Currency tokenCurrency;
+
+    PointsHook hook;
+
+    function setUp() public {
+        deployFreshManagerAndRouters();
+
+        token = new MockERC20("Test Token", "TEST", 18);
+        tokenCurrency = Currency.wrap(address(token));
+
+        token.mint(address(this), 100 ether);
+        token.mint(address(1), 100 ether);
+
+        uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG);
+        // deploy the hook contract
+        deployCodeTo("PointsHook.sol:PointsHook", abi.encode(manager), address(flags));
+
+        // Deploy the hook
+        hook = PointsHook(address(flags));
+
+        // Approve our TEST token for spending
+        token.approve(address(swapRouter), type(uint256).max);
+        token.approve(address(modifyLiquidityRouter), type(uint256).max);
+
+        // Initialize a pool
+        (key, ) = initPool(
+            ethCurrency,
+            tokenCurrency,
+            hook,
+            3000,
+            SQRT_PRICE_1_1
+        );
+
+        // Add some liquidity to the pool
+        uint160 sqrtPriceAtTickLower = TickMath.getSqrtPriceAtTick(-60);
+        uint160 sqrtPriceAtTickUpper = TickMath.getSqrtPriceAtTick(60);
+
+        uint256 ethToAdd = 0.003 ether;
+        uint128 liquidityDelta = LiquidityAmounts.getLiquidityForAmount0(
+            SQRT_PRICE_1_1,
+            sqrtPriceAtTickUpper,
+            ethToAdd
+        );
+
+        console.log("liquidityDelta:", liquidityDelta);
+
+        uint256 tokenToAdd = LiquidityAmounts.getAmount1ForLiquidity(
+            sqrtPriceAtTickLower,
+            SQRT_PRICE_1_1,
+            liquidityDelta
+        );
+
+        console.log("amount1:", tokenToAdd);
+
+        modifyLiquidityRouter.modifyLiquidity{value: ethToAdd}(
+            key,
+            ModifyLiquidityParams({
+                tickLower: -60,
+                tickUpper: 60,
+                liquidityDelta: int256(uint256(liquidityDelta)),
+                salt: bytes32(0)
+            }),
+            ZERO_BYTES
+        );
+    }
+
+    function test_swap() public {
+        uint256 poolIdUint = uint256(PoolId.unwrap(key.toId()));
+        uint256 pointsBalanceOriginal = hook.balanceOf(address(this), poolIdUint);
+
+        // Set user addr
+        bytes memory hookData = abi.encode(address(this));
+
+        swapRouter.swap{ value: 0.001 ether}(
+            key,
+            SwapParams({
+                zeroForOne: true,
+                amountSpecified: -0.001 ether,
+                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+            }),
+            PoolSwapTest.TestSettings({
+                takeClaims: false,
+                settleUsingBurn: false
+            }),
+            hookData
+        );
+
+        uint256 pointsBalanceAfterSwap = hook.balanceOf(address(this), poolIdUint);
+
+        // We should get 20% of 0.001 * 10**18 points
+        // = 2 * 10**14
+        assertEq(pointsBalanceAfterSwap - pointsBalanceOriginal, 2 * 10 ** 14);
+    }
+
+}
