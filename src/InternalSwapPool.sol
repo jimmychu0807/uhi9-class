102486826c8508facd2031ab14bb3d7eb0d5fd45
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2026-05-08 20:43:27 +0800

    updated

--- src/InternalSwapPool.sol
+++ src/InternalSwapPool.sol
@@ -54,6 +54,55 @@ contract InternalSwapPool is BaseHook {
         override
         returns (bytes4 selector_, BeforeSwapDelta beforeSwapDelta_, uint24 swapFee_)
     {
+        PoolId poolId = key.toId();
+
+        if (params.zeroForOne && _poolFees[poolId].amount1 != 0) {
+            uint256 tokenIn;
+            uint256 ethOut;
+
+            (uint160 sqrtPriceX96,,,) = poolManager.getSlot0(poolId);
+
+            if (params.amountSpecified >= 0) {
+                uint256 amountSpecified = (uint256(params.amountSpecified) > _poolFees[poolId].amount1)
+                    ? _poolFees[poolId].amount1
+                    : uint256(params.amountSpecified);
+
+                (, ethOut, tokenIn,) = SwapMath.computeSwapStep({
+                    sqrtPriceCurrentX96: sqrtPriceX96,
+                    sqrtPriceTargetX96: params.sqrtPriceLimitX96,
+                    liquidity: poolManager.getLiquidity(poolId),
+                    amountRemaining: int256(amountSpecified),
+                    feePips: 0
+                });
+
+                beforeSwapDelta_ = toBeforeSwapDelta(-int128(int256(tokenIn)), int128(int256(ethOut)));
+            } else {
+                (, ethOut, tokenIn,) = SwapMath.computeSwapStep({
+                    sqrtPriceCurrentX96: sqrtPriceX96,
+                    sqrtPriceTargetX96: params.sqrtPriceLimitX96,
+                    liquidity: poolManager.getLiquidity(poolId),
+                    amountRemaining: int256(_poolFees[poolId].amount1),
+                    feePips: 0
+                });
+
+                if (ethOut > uint256(-params.amountSpecified)) {
+                    uint256 percentage = (uint256(-params.amountSpecified) * 1e18) / ethOut;
+                    tokenIn = (tokenIn * percentage) / 1e18;
+                }
+
+                beforeSwapDelta_ = toBeforeSwapDelta(int128(int256(ethOut)), -int128(int256(tokenIn)));
+            }
+
+            _poolFees[poolId].amount0 += ethOut;
+            _poolFees[poolId].amount1 -= tokenIn;
+
+            poolManager.sync(key.currency0);
+            poolManager.sync(key.currency1);
+
+            poolManager.take(key.currency0, address(this), ethOut);
+            key.currency1.settle(poolManager, address(this), tokenIn, false);
+        }
+
         selector_ = IHooks.beforeSwap.selector;
     }
 
@@ -64,10 +113,38 @@ contract InternalSwapPool is BaseHook {
         BalanceDelta delta,
         bytes calldata hookData
     ) internal override returns (bytes4 selector_, int128 hookDeltaUnspecified_) {
+        Currency swapFeeCurrency = (params.amountSpecified < 0) == params.zeroForOne ? key.currency1 : key.currency0;
+
+        int128 swapAmount = params.amountSpecified < 0 == params.zeroForOne ? delta.amount1() : delta.amount0();
+
+        uint256 swapFee = uint256(uint128(swapAmount < 0 ? -swapAmount : swapAmount)) * 99 / 100;
+
+        depositFees(key, params.zeroForOne ? swapFee : 0, params.zeroForOne ? 0 : swapFee);
+
+        swapFeeCurrency.take(poolManager, address(this), swapFee, false);
+
+        hookDeltaUnspecified_ = -int128(int256(swapFee));
+
+        _distributeFees(key);
         selector_ = IHooks.afterSwap.selector;
     }
 
-    function _distributeFees(PoolKey calldata _poolKey) internal {}
+    function _distributeFees(PoolKey calldata _poolKey) internal {
+        PoolId poolId = _poolKey.toId();
+        uint256 donateAmount = _poolFees[poolId].amount0;
+
+        if (donateAmount < DONATE_THRESHOLD_MIN) {
+            return;
+        }
+
+        BalanceDelta delta = poolManager.donate(_poolKey, donateAmount, 0, "");
+
+        if (delta.amount0() < 0) {
+            _poolKey.currency0.settle(poolManager, address(this), uint256(uint128(-delta.amount0())), false);
+        }
+
+        _poolFees[poolId].amount0 -= donateAmount;
+    }
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
